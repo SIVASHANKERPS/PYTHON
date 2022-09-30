1a62754196b40ba35b907cddeb5ce5832bcc9fb0
SIVA SHANKER PS <112741003+SIVASHANKERPS@users.noreply.github.com>
2022-09-30 23:06:55 +0530

    Add files via upload

---
+++ assignment 2.py
@@ -0,0 +1,82 @@
+#Calculate the multiplication and sum of two numbers
+# a=10
+# b=30
+# print(a*b,a+b)
+
+# Print the sum of the current number and the previous number
+# current=input("enter a number")
+# prev=input("enter a number")
+# sum=int(current)+int(prev)
+# print("currrent no:",current + "previous no:",prev + "sum:",sum)
+
+# Print a string if it has a particular character
+# str="siva shanker"
+# if "a" in str:
+#     print(str)
+# else:
+#     print("not present")
+
+# Check if the first and last number of a list is the same
+# lst=[1,2,3,4,5,1]
+# if lst[0]==lst[-1]:
+#     print("true")
+# else:
+#     print("false")
+
+# Return a Python list of all the even numbers upto n using function
+# for num in range(2,20,2):
+#  print(num)
+ 
+#  function to return appended  string in the middle of a given string
+
+# Write a program to find the LCM of two numbers.
+# a=50
+# b=20
+# if (a>b):
+#     greater=a
+# else:
+#     greater=b
+# while(True):
+#     if (greater%a==0 and greater%b==0):
+#      print("the lcm is",greater)
+#      break;
+# greater=greater+1
+
+# Write a program to check if the given year is a leap year.
+# YEAR=int(input("enter a year"))
+# if YEAR%4==0:
+#     print("it is a leap year")
+# else:
+#     print("it is not a leap year")
+
+# Write a function that removes the vowels from a string.
+
+# Create a function removes repeated letters from a string.
+# string="sivashanker"
+# p=""
+# for char in string:
+#     if char not in p:
+#         p=p+char
+# print(p)
+
+#check if 2 lists are the same.
+# lst1=[1,2,3,4,5]
+# lst2=[1,2,3,4,5]
+# if set(lst1)==set(lst2):
+#     print("true")
+# else:
+#     print("false")
+
+# Insert an element into a list at a particular position.
+# lst=[1,2,3,4,5]
+# num=6
+# index=2
+# lst.insert(index,num)
+# print(lst)
+
+# Rotate the elements of a list by two positions
+# lst=[1,2,3,4,5]
+# lst=lst[2:]+lst[:2]
+# print(lst)
+
+    
